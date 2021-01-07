97cfe7939dbad0a0c69eab03f60fd32a2514cfb2
michalkoziarski <michal.koziarski@gmail.com>
2021-01-08 00:26:29 +0100

    renamed scripts

--- schedule_ensemble_full.py
+++ schedule_ensemble_full.py
@@ -3,6 +3,6 @@ import os
 
 if __name__ == '__main__':
     for fold in range(10):
-        command = f'sbatch run.sh train_ensemble.py -fold {fold}'
+        command = f'sbatch run.sh train_ensemble_full.py -fold {fold}'
 
         os.system(command)
